9af84c039e08f23b2c2a7104c909aed632a9ae5e
stefankarlbergz <stefan.karlberg@zooma.se>
2019-11-20 20:25:24 +0100

    Refactoring

---
+++ src/components/Home.vue
@@ -0,0 +1,101 @@
+<template>
+    <div class="starship">
+        <h1>Welcome to Starship Junkyard</h1>
+        <div class="starship-wrapper">
+            <div class="starship-wrapper__card" v-for="item in starships" :key="item.model">
+                <div class="starship-wrapper__card__overlay">
+                    <h3><a href="#" v-on:click="loadComponent">{{ item.name }}</a></h3>
+                    <h4>{{ item.model}}</h4>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Home",
+        props: ['starships'],
+        methods: {
+            loadComponent() {
+                this.$emit('loadComponent1')
+                window.console.log('test')
+            }
+        },
+    }
+</script>
+
+<style scoped lang="scss">
+
+    .starship {
+        width: 80%;
+        margin-left: auto;
+        margin-right: auto;
+
+    h1 {
+        font-family: arial;
+        text-transform: uppercase;
+        letter-spacing: 3px;
+        font-weight: 300;
+        font-size: 40px;
+        margin-top: 100px;
+        margin-bottom: 100px;
+        color: green;
+        }
+    }
+
+    .starship-wrapper {
+        display: flex;
+        flex-wrap: wrap;
+        justify-content: center;
+
+    &__card {
+         display: flex;
+         align-items: flex-end;
+         width: 300px;
+         height: 200px;
+         margin: 10px;
+         border: 1px solid green;
+         background: black;
+
+    &__overlay {
+         height: 100%;
+         width: 100%;
+         display: flex;
+         flex-direction: column;
+         align-items: center;
+         justify-content: center;
+
+    &:hover {
+         background: green;
+     }
+
+    h3 a {
+        font-size: 25px;
+        font-family: arial;
+        text-transform: uppercase;
+        color: white;
+        margin-bottom: 0px;
+        text-decoration: none;
+    }
+
+    h4 {
+        font-size: 12px;
+        font-family: arial;
+        color: white;
+        margin-top: 0px;
+    }
+    }
+    }
+    }
+
+
+    .sidebar-open {
+        position: fixed;
+        overflow: hidden;
+
+    .main {}
+    .sidebar {}
+    }
+
+</style>
---
+++ src/components/SpareParts.vue
@@ -0,0 +1,109 @@
+<template>
+    <div class="spare-parts">
+        <div class="spare-parts__hotspot">
+            <div class="spare-parts__quantity">
+                <h2>Forward <br /> deflector shields</h2>
+                <button class="add" v-on:click="increaseItems">+</button>
+                <button class="total">{{ totalitems }}</button>
+                <button class="remove" v-on:click="decreaseItems">-</button>
+            </div>
+            <div class="spare-parts__add-to-cart">
+                <button v-on:click="addToCart">Add to cart</button>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "SpareParts",
+        props: ['totalitems'],
+        methods: {
+            increaseItems() {
+                this.$emit('increase-items')
+            },
+            decreaseItems() {
+                this.$emit('decrease-items')
+            },
+            addToCart() {
+                this.$emit('add-to-cart', 'totalItems')
+            },
+        },
+    }
+
+</script>
+
+<style scoped lang="scss">
+
+    .spare-parts {
+        width: 100%;
+        height: 100vh;
+        background: url("http://stefankarlberg.se/wp-content/uploads/2019/11/falcon.jpg");
+        background-repeat: no-repeat;
+        background-position: center center;
+        background-size: cover;
+
+        &__hotspot {
+            right: 20%;
+            top: 10%;
+            height: 250px;
+            width: 250px;
+            border-radius: 50%;
+            position: absolute;
+            background: rgba(27, 60, 3, 0.93);
+        }
+
+        &__quantity {
+            margin-top: 20px;
+            padding: 10px;
+
+            &.locked {
+            pointer-events: none;
+        }
+    }
+
+    h2 {
+        margin-bottom: 20px;
+        font-size: 17px;
+        color: white;
+        line-height: 20px;
+        text-transform: uppercase;
+        font-family: arial;
+    }
+
+    $mobile-side-margin: 10px;
+    $desktop-side-margin: 20px;
+
+    button {
+        padding: 5px $mobile-side-margin;
+        margin: 0px;
+        color: white;
+        background: none;
+        border: 1px solid white;
+        font-size: 17px;
+        list-style: 17px;
+
+    @media (min-width: 480px) {
+        padding-left: $desktop-side-margin;
+        padding-right: $desktop-side-margin;
+        }
+    }
+
+    &__add-to-cart {
+         margin-top: 20px;
+
+    button {
+        color: white;
+        background: none;
+        font-size: 20px;
+        padding: 7px;
+        border: 1px solid white;
+    }
+
+    button:hover {
+        color: black;
+        background: white;
+    }
+    }
+    }
+</style>
